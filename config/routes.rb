70cc573b5403f5a25120a1c7e3f46bb3ab87f597
[jsw324] <jwalkow@tallience.com>
2016-11-22 16:10:29 -0500

    committing for collaboration

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
     get :admin, on: :collection
     get :adminShow, on: :collection
   end
+
   get 'home/index'
   root 'home#index'
   get 'days/show'
   get 'days/admin', to: 'days#admin', as: :admin
+
 
 
   post 'days/new'

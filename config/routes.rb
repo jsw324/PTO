83f3ed45d7497463fb936bcb0d3e39a9665deef6
[jsw324] <jwalkow@tallience.com>
2016-12-12 16:04:49 -0500

    fixed update

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
     get :adminShow, on: :collection
   end
 
+resources :days
   get 'home/index'
   root 'home#index'
   get 'days/show'
   get 'days/admin', to: 'days#admin', as: :admin
-
+  put 'days/:id', to: 'days#update'
+  patch 'days/:id', to: 'days#update'
 
 
   post 'days/new'
